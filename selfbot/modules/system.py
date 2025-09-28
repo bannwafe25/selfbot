a8331c52e7c53d04fb80f81f7412c18f1f36ab95
beepsound <219347502+beepsound@users.noreply.github.com>
2025-09-28 14:17:14 +0700

    chore(selfbot.modules.system): inline respond

--- selfbot/modules/system.py
+++ selfbot/modules/system.py
@@ -77,9 +77,7 @@ class System(Module):
                 InlineQueryResultCachedSticker(
                     sticker_file_id=self.client.config["sticker_file_id"],
                     reply_markup=ikm((">_", "user_id", event._client.me.id)),
-                    input_message_content=InputTextMessageContent(
-                        "<code>Update and Restart...</code>"
-                    ),
+                    input_message_content=InputTextMessageContent("<code>...</code>"),
                 )
             ],
             cache_time=900,
