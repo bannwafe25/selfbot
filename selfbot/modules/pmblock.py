bf8e0f2ae7423fc00e0c183efe28d975e2a397ac
beepsound <219347502+beepsound@users.noreply.github.com>
2025-09-29 19:09:10 +0700

    fix(selfbot.modules.pmblock): logic auth/unauth (2)

--- selfbot/modules/pmblock.py
+++ selfbot/modules/pmblock.py
@@ -233,8 +233,8 @@ class PmBlock(Module):
             auth = await self.client.db.fetchval(
                 "SELECT auth FROM pmblock_auths WHERE user_id = $1;", data["user"]
             )
-            if (not auth and data["action"].startswith("un")) or (
-                auth and data["action"] == "auth"
+            if (auth is True and data["action"] == "auth") or (
+                auth is False and data["action"] == "unauth"
             ):
                 pass
             else:
