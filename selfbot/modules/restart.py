c828c20a8f76f6cd54d25225b2705dfde3b06b4d
beepsound <219347502+beepsound@users.noreply.github.com>
2025-10-17 03:57:55 +0700

    Revert "chore(deps): deprecate requirements.txt in favor of poetry.lock"
    This reverts commit 13babbd26cd2aab3ef80d131540f4037b3b09209.

--- selfbot/modules/restart.py
+++ selfbot/modules/restart.py
@@ -2,7 +2,6 @@ import asyncio
 import json
 import os
 import re
-import subprocess
 import sys
 
 import git
@@ -47,7 +46,7 @@ class Restart(Module):
     async def on_message_out(self, event: Message) -> None:
         await event.edit_text("<code>...</code>")
 
-        def fetch(repo, remote) -> None:
+        def fetch(repo, remote):
             origin = next((r for r in repo.remotes if r.name == "origin"), None)
             if origin is None:
                 origin = repo.create_remote("origin", remote)
@@ -60,15 +59,15 @@ class Restart(Module):
 
             origin.fetch(prune=True)
 
-        def check() -> bool:
+        def check():
             repo = git.Repo(".") if os.path.isdir(".git") else git.Repo.init(".")
-
-            remote = self.client.config.get(
+            remote_url = self.client.config.get(
                 "remote", "https://github.com/DeltaUniverse/selfbot"
             ).removesuffix(".git")
             branch = self.client.config.get("branch", "staging")
 
-            fetch(repo, remote)
+            fetch(repo, remote_url)
+            repo.git.reset("--hard", f"origin/{branch}")
 
             old = repo.head.commit.hexsha
             new = repo.commit(f"origin/{branch}").hexsha
@@ -76,27 +75,31 @@ class Restart(Module):
             if old == new:
                 return False
 
-            repo.git.reset("--hard", f"origin/{branch}")
-
+            deps = {"requirements.txt", "pyproject.toml", "poetry.lock"}
             for diff in repo.commit(old).diff(new):
-                if (diff.a_path or "").lower() == "poetry.lock" or (
+                if (diff.a_path or "").lower() in deps or (
                     diff.b_path or ""
-                ).lower() == "poetry.lock":
+                ).lower() in deps:
                     return True
 
             return False
 
         changed = await asyncio.to_thread(check)
+
         if changed:
             await event.edit_text("<code>Updating...</code>")
 
             def update():
                 try:
-                    subprocess.run(["poetry", "--version"], check=True)
-                    subprocess.run(["poetry", "self", "update"], check=True)
-                    subprocess.run(["poetry", "install", "--all-extras"], check=True)
-                except Exception as e:
-                    self.logger.error(f"{e.__class__.__name__}: {e}")
+                    from pip._internal.cli.main import main as pip
+
+                    pip(["install", "--upgrade", "pip", "setuptools", "wheel"])
+                    if os.path.exists("requirements.txt"):
+                        pip(["install", "-r", "requirements.txt"])
+                    elif os.path.exists("pyproject.toml"):
+                        pip(["install", "."])
+                except Exception:
+                    pass
 
             await asyncio.to_thread(update)
 
