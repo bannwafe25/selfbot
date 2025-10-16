64e8202ec5b4e5108fe27be98ab5187eacd7e137
beepsound <219347502+beepsound@users.noreply.github.com>
2025-10-17 03:50:48 +0700

    fix(selfbot.modules.restart): ValueError Reference at 'refs/heads/master' does not exist

--- selfbot/modules/restart.py
+++ selfbot/modules/restart.py
@@ -69,6 +69,7 @@ class Restart(Module):
             branch = self.client.config.get("branch", "staging")
 
             fetch(repo, remote)
+            repo.git.reset("--hard", f"origin/{branch}")
 
             old = repo.head.commit.hexsha
             new = repo.commit(f"origin/{branch}").hexsha
