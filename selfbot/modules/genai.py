2b557d128872f9997b14ab0246b46a0916881c9b
beepsound <219347502+beepsound@users.noreply.github.com>
2025-10-24 14:34:17 +0700

    feat(selfbot.modules.genai): fallback unsupported mime_type

--- selfbot/modules/genai.py
+++ selfbot/modules/genai.py
@@ -153,6 +153,13 @@ class GenAI(Module):
                     if obj.file_size > 8 * 1024**2:
                         return await edit("<code>Media too Large (Limit: 8 MB)</code>")
 
+                    if not obj.mime_type.startswith(
+                        ("audio", "image", "video")
+                    ) or obj.mime_type not in ["text/plain", "application/pdf"]:
+                        return await edit(
+                            f"<code>Unsupported '{obj.mime_type}' MIME Type</code>"
+                        )
+
                     doc = await event._client.download_media(
                         (
                             event.reply_to_message
