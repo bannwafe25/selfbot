0fc8a819c2ac3187dcd73cb023a545954ff27563
beepsound <219347502+beepsound@users.noreply.github.com>
2025-10-24 14:49:02 +0700

    fix(selfbot.modules.genai): mime_type

--- selfbot/modules/genai.py
+++ selfbot/modules/genai.py
@@ -153,9 +153,10 @@ class GenAI(Module):
                     if obj.file_size > 8 * 1024**2:
                         return await edit("<code>Media too Large (Limit: 8 MB)</code>")
 
+                    m_t = obj.mime_type.lower().strip()
                     if not isinstance(obj, Sticker) and (
-                        not obj.mime_type.startswith(("audio", "image", "video"))
-                        or obj.mime_type not in ["text/plain", "application/pdf"]
+                        not m_t.startswith(("audio", "image", "video"))
+                        or m_t not in ["text/plain", "application/pdf"]
                     ):
                         return await edit(
                             f"<code>Unsupported '{obj.mime_type}' MIME Type</code>"
